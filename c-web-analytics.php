c04c1e6c79677f576d391242efb3dc40f266d0ae
Jakub Bebjak <bebjakub@gmail.com>
2022-06-13 13:12:49 +0200

    1.0.3

--- c-web-analytics.php
+++ c-web-analytics.php
@@ -6,7 +6,7 @@
  * Plugin URI:          https://github.com/gdidentity/c-web-analytics
  * GitHub Plugin URI:   https://github.com/gdidentity/c-web-analytics
  * Description:         Cloudflare Web Analytics in your WordPress admin.
- * Version:             1.0.2
+ * Version:             1.0.3
  * Author:              GD IDENTITY
  * Author URI:          https://gdidentity.sk
  * Text Domain:         cwa
@@ -119,7 +119,7 @@ if ( ! class_exists( 'CWebAnalytics' ) ) :
 
 			// Plugin version.
 			if ( ! defined( 'CWEBANALYTICS_VERSION' ) ) {
-				define( 'CWEBANALYTICS_VERSION', '1.0.2' );
+				define( 'CWEBANALYTICS_VERSION', '1.0.3' );
 			}
 
 			// Plugin Folder Path.
