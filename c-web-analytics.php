22a960609e91e2d29563bdb30a338e0968e97fa7
Jakub Bebjak <bebjakub@gmail.com>
2022-06-16 11:41:53 +0200

    remove vendor

--- c-web-analytics.php
+++ c-web-analytics.php
@@ -6,7 +6,7 @@
  * Plugin URI:          https://github.com/gdidentity/c-web-analytics
  * GitHub Plugin URI:   https://github.com/gdidentity/c-web-analytics
  * Description:         Cloudflare Web Analytics in your WordPress admin.
- * Version:             1.0.6
+ * Version:             1.0.7
  * Author:              GD IDENTITY
  * Author URI:          https://gdidentity.sk
  * Text Domain:         cwa
@@ -119,7 +119,7 @@ if ( ! class_exists( 'CWebAnalytics' ) ) :
 
 			// Plugin version.
 			if ( ! defined( 'CWEBANALYTICS_VERSION' ) ) {
-				define( 'CWEBANALYTICS_VERSION', '1.0.6' );
+				define( 'CWEBANALYTICS_VERSION', '1.0.7' );
 			}
 
 			// Plugin Folder Path.
